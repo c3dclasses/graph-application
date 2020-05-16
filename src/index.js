46e50ce84ad14012dcc3d5492a6458da551dd167
c3dclasses <c3dclasses@gmail.com>
2020-05-16 02:40:44 -0400

    initial commit of the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from "./components/app/app";
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import GraphApplicationReducer from "./reducers/graphapplicationreducer";
+
+// create the graph application store
+const graphappstore = createStore(GraphApplicationReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={graphappstore}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
